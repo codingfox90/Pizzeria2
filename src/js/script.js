494be4afbbef495be6fcadfc8b1f4604086b21bd
Kuba B <geralt9500@o2.pl>
2022-06-25 11:38:45 +0200

    fix activeProduct bug

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordin();
     }
     renderInMenu() {
@@ -77,25 +78,39 @@
       /*add element to menu*/
       menuContainer.appendChild(thisProduct.element);
     }
+    getElements() {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
+      thisProduct.form = thisProduct.element.querySelector(
+        select.menuProduct.form
+      );
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(
+        select.all.formInputs
+      );
+      thisProduct.cartButton = thisProduct.element.querySelector(
+        select.menuProduct.cartButton
+      );
+      thisProduct.priceElem = thisProduct.element.querySelector(
+        select.menuProduct.priceElem
+      );
+    }
     initAccordin() {
       const thisProduct = this;
       console.log('thisProduct', thisProduct);
       console.log('thisProductelement!!!', thisProduct.element);
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(
-        select.menuProduct.clickable
-      );
-
-      console.log('clickableTrigger', clickableTrigger);
 
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(
-          classNames.menuProduct.wrapperActive
+          '.' + classNames.menuProduct.wrapperActive
         );
         console.log(
           'classNames.menuProduct.wrapperActive:',
